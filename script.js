f5397757c9949be46d49c9f32708cb09f1c9284a
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-06-04 22:02:32 +0500

    Updates allignment and text

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ let HeadingColorReChange = () => {
 
 let FontChange = () => {
   myfirstheading.position = "absolute";
-  myfirstheading.style.color = "#75A1BE";
-  myfirstheading.style.fontSize = "40";
+  myfirstheading.style.color = "grey";
+  myfirstheading.style.fontSize = "51";
+  let x = document.getElementById('Ghost-text');
+  x.innerText = "Did you miss your friends? Or did you learn something?";
+
 }
 
 let FontReChange = () => {
   myfirstheading.position = "absolute";
   myfirstheading.style.color = "white";
   myfirstheading.style.fontSize = "45";
+  document.getElementById('Ghost-text').innerText = "";
+
 }
 
 let myfirstheading = document.getElementById('Heading-1');
@@ -27,10 +32,14 @@ myfirstheading.addEventListener('mouseleave', FontReChange)
 
 
 let SubHeadingColorChange = () => {
-  subheading.style.color = "black";
+  subheading.style.color = "grey";
+  let y = document.getElementById('Ghost-text-2');
+  y.innerText = "Can you imagine that it took us only thirty minutes to come up with this?";
 }
 let SubHeadingColorReChange = () => {
-  subheading.style.color = "75A1BE";
+  subheading.style.color = "white";
+  let y = document.getElementById('Ghost-text-2');
+  y.innerText = "";
 }
 
 let subheading = document.getElementById('Heading-2');
