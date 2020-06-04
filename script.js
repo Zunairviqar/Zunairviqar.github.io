b6434aec982d00aad7d8f76706f9cf4ba66b0df9
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-06-04 05:25:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ let HeadingColorChange = () => {
   myfirstheading.style.color = "black";
 }
 let HeadingColorReChange = () => {
-  myfirstheading.style.color = "75A1BE";
+  myfirstheading.position = "absolute";
+  myfirstheading.style.fontSize = "30";
+}
+
+let FontChange = () => {
+  myfirstheading.position = "absolute";
+  myfirstheading.style.color = "black";
+  myfirstheading.style.fontSize = "40";
+}
+
+let FontReChange = () => {
+  myfirstheading.position = "absolute";
+  myfirstheading.style.color = "#75A1BE";
+  myfirstheading.style.fontSize = "30";
 }
 
 let myfirstheading = document.getElementById('Heading-1');
-myfirstheading.addEventListener('mouseenter',HeadingColorChange)
-myfirstheading.addEventListener('mouseleave', HeadingColorReChange)
+// myfirstheading.addEventListener('mouseenter',HeadingColorChange)
+// myfirstheading.addEventListener('mouseenter',HeadingColorReChange)
+myfirstheading.addEventListener('mouseenter',FontChange)
+myfirstheading.addEventListener('mouseleave', FontReChange)
+
 
 let SubHeadingColorChange = () => {
   subheading.style.color = "black";
